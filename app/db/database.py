33a8126e7f8b4e2da19e7ededa318d6cc83754a6
Robert028 <rimniceanu.robert38@gmail.com>
2026-05-22 10:51:33 +0300

    fix: remove SQLite fallback, add PostgreSQL to docker-compose

--- app/db/database.py
+++ app/db/database.py
@@ -3,10 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "sqlite:///./app/db/insideugal.db"
-)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
