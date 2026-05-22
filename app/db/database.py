cb3530e83b26189f149becbbd9345022831d8455
Robert028 <rimniceanu.robert38@gmail.com>
2026-05-22 08:28:01 +0300

    chore: migrate from SQLite to PostgreSQL, remove db from git

--- app/db/database.py
+++ app/db/database.py
@@ -1,12 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./app/db/insideugal.db"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./app/db/insideugal.db"
+)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 
