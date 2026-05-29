4310024c918dda7ef27c40775224a833429fd102
Andreea <costinandreea2004@gmail.com>
2026-05-29 14:43:06 +0300

    Creare sidebar

---
+++ Frontend/Dashboard/dashboard-insideugal/app/harti/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div className="text-h1 text-neutral-900 font-bold p-4">Pagină Harti în lucru...</div>;
+}
--- Frontend/Dashboard/dashboard-insideugal/app/test/sanity.test.ts
+++
@@ -1,5 +0,0 @@
-import { expect, test } from 'vitest'
-
-test('Sanity check: environment is set up correctly', () => {
-  expect(1 + 1).toBe(2)
-})
