c21454a37acd6094e843281e2c7c5a8789fe0600
Lungu Laurentiu <laurentiu2906@gmail.com>
2026-06-08 14:20:26 +0300

    fix: harta fisier config

--- Frontend/Dashboard/dashboard-insideugal/app/components/MapComponent.tsx
+++ Frontend/Dashboard/dashboard-insideugal/app/components/MapComponent.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import Map, { Marker, MapLayerMouseEvent } from "react-map-gl/maplibre";
-import { Config } from "../../lib/config";
+import { Config } from "@/lib/config";
 import "maplibre-gl/dist/maplibre-gl.css";
 
 export default function MapComponent({ onLocationSelect }: { onLocationSelect: (lat: number, lng: number) => void }) {
