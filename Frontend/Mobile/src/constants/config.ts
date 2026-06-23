67b1cf55fa0e8f35d97e354aeecb76171214631b
alexurrc18 <alexandru.calin18@icloud.com>
2026-06-23 11:23:29 +0300

    fix: schimbare format erori

--- Frontend/Mobile/src/constants/config.ts
+++ Frontend/Mobile/src/constants/config.ts
@@ -1,5 +1,6 @@
 export const Config = {
-  MAPTILER_STYLE_URL: 'https://api.maptiler.com/maps/019ea17c-89bb-7ed5-b99b-850727394299/style.json?key=O12wVsNDEy2SNlkC8eYS',
-  API_BASE_URL: 'http://10.66.4.9:8002',
-  DASHBOARD_URL: 'https://dashboard.insideugal.ro',
+  MAPTILER_STYLE_URL: process.env.EXPO_PUBLIC_MAPTILER_STYLE_URL,
+  API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL,
+  LLM_BASE_URL: process.env.EXPO_PUBLIC_LLM_BASE_URL,
+  DASHBOARD_URL: process.env.EXPO_PUBLIC_DASHBOARD_URL,
 };
